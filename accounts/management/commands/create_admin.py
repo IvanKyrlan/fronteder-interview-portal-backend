6ebf5e10f12df27cdd403f548e114273e9113fb8
IvanKyrlan <kyrlan.ivan@chnu.edu.ua>
2025-05-28 18:15:19 +0300

    commands fix

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -1,15 +1,12 @@
-# create_admin.py
-
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
-
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
-    print("Admin created!")
-else:
-    print("Admin already exists!")
+class Command(BaseCommand):
+    help = 'Create an admin user automatically'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
+            self.stdout.write(self.style.SUCCESS('Admin created!'))
+        else:
+            self.stdout.write(self.style.WARNING('Admin already exists!'))
