f034efe4819a20dc7411ad0d0f126521fcd94cc6
IvanKyrlan <kyrlan.ivan@chnu.edu.ua>
2025-06-18 12:28:41 +0300

    add admin logs

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'forum',
     'interviews',
     'comments',
+    'core',
 ]
 
 REST_FRAMEWORK = {
--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,13 @@
-# core/admin.py
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = [f.name for f in LogEntry._meta.fields]  # не дозволяємо редагування
+    list_display = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
+    list_filter = ('user', 'content_type', 'action_flag')
+    search_fields = ('object_repr', 'change_message')
+    ordering = ('-action_time',)
+
+    def has_add_permission(self, request):
+        return False  # забороняємо створення вручну
